6294b8b1317c726d6ec4a44333862b566a80dc21
kennethraff <94583510+kennethraff@users.noreply.github.com>
2023-04-11 08:40:36 +0700

    Section 2

--- AFL3-0706012110005-Kenneth Raffelino S/ContentView.swift
+++ AFL3-0706012110005-Kenneth Raffelino S/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, SwiftUI!")
+            Text("Turtle Rock")
+                .font(.title).foregroundColor(.black)
         }
         .padding()
     }
