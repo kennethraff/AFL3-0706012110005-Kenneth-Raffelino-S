dcf3deb8a6f9fdb6474eeca9ffd07f45b40dd840
kennethraff <94583510+kennethraff@users.noreply.github.com>
2023-04-11 09:08:53 +0700

    Section 6

--- AFL3-0706012110005-Kenneth Raffelino S/ContentView.swift
+++ AFL3-0706012110005-Kenneth Raffelino S/ContentView.swift
@@ -10,9 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+            MapView().frame(height: 300).ignoresSafeArea(edges: .top)
+
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+            CircleImage()
+                            .offset(y: -130)
+                            .padding(.bottom, -130)
             VStack (alignment: .leading){
                 Text("Turtle Rock")
                     .font(.title).foregroundColor(.black)
@@ -23,6 +28,14 @@ struct ContentView: View {
                     Text("California")
                         .font(.subheadline)
                 }
+                .font(.subheadline)
+                                .foregroundColor(.secondary)
+
+                Divider()
+
+                                Text("About Turtle Rock")
+                                    .font(.title2)
+                                Text("Descriptive text goes here.")
             }
         }
         .padding()
