86af88eb165797b4ed222561cc316ca8fcec33c7
kennethraff <94583510+kennethraff@users.noreply.github.com>
2023-04-11 08:28:40 +0700

    Initial Commit

---
+++ AFL3-0706012110005-Kenneth Raffelino S/AFL3_0706012110005_Kenneth_Raffelino_SApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFL3_0706012110005_Kenneth_Raffelino_SApp.swift
+//  AFL3-0706012110005-Kenneth Raffelino S
+//
+//  Created by MacBook Pro on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AFL3_0706012110005_Kenneth_Raffelino_SApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
