0be2f8366265cfbc32721b082714e39734244e19
kennethraff <94583510+kennethraff@users.noreply.github.com>
2023-04-18 09:15:38 +0700

    Chapter 3 done

--- AFL3-0706012110005-Kenneth Raffelino S/Views/Category/CategoryHome.swift
+++ AFL3-0706012110005-Kenneth Raffelino S/Views/Category/CategoryHome.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct CategoryHome: View {
     //create modelData
     @EnvironmentObject var modelData: ModelData
+    @State private var showingProfile = false
     var body: some View {
         //buat kek div nya
         NavigationView {
@@ -28,7 +29,19 @@ struct CategoryHome: View {
             .listRowInsets(EdgeInsets())
                 
             }
+            .listStyle(.inset)
             .navigationTitle("Featured")
+            .toolbar {
+                           Button {
+                               showingProfile.toggle()
+                           } label: {
+                               Label("User Profile", systemImage: "person.crop.circle")
+                           }
+                       }
+                       .sheet(isPresented: $showingProfile) {
+                           ProfileHost()
+                               .environmentObject(modelData)
+                       }
         }
     }
 }
