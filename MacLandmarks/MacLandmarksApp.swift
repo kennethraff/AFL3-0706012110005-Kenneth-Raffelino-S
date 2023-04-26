cbab0ca8cbeeefb04e595f20223a815a0dfc8859
kennethraff <94583510+kennethraff@users.noreply.github.com>
2023-04-26 23:28:15 +0700

    macOS step 4 done

---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by MacBook Pro on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
