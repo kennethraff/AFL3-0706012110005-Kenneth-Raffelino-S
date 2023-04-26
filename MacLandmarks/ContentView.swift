6c1aa431ad65e83295034249d3853aa105f6c806
kennethraff <94583510+kennethraff@users.noreply.github.com>
2023-04-26 23:49:05 +0700

    macOS sec 1

--- MacLandmarks/ContentView.swift
+++ MacLandmarks/ContentView.swift
@@ -6,21 +6,16 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }
